f66d51154c1eaaa524f0a250c5cfc946357878f7
Deepak Singla <thejavascriptways@gmail.com>
2017-05-03 01:16:56 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HowToComponent } from './how-to/how-to.component';
+import { ExamplesComponent } from './examples/examples.component';
+
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HowToComponent,
+    ExamplesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
