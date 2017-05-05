9fdfd5485f3847bcd81d7a97be76c19aa89cc2b9
Deepak Singla <thejavascriptways@gmail.com>
2017-05-05 01:37:26 -0500

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, trigger, style, state,transition, animate, keyframes, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('focusPanel', [
+      state('inactive', style({
+        transform: 'scale(1)'
+      })),
+     state('active', style({
+       transform: 'scale(1.04)'
+     })),
+     transition('inactive => active', animate('500ms ease-in')),
+     transition('active => inactive', animate('500ms ease-out'))
+    ]),
+    trigger('note', [
+      state('inactive', style({
+        opacity:'0'
+      })),
+      state('active', style({
+        opacity:'1'
+      })),
+      transition('inactive => active',[
+        animate(300, keyframes([
+          style({opacity:0, transform: 'translateY(0)', offset:0}),
+          style({opacity:1, transform: 'translateY(-15px)', offset:0.6}),
+          style({opacity:1, transform: 'translateY(0)', offset:0}),
+        ]))
+      ]),
+      transition('active => inactive',[
+        animate(300, keyframes([
+          style({opacity:1, transform: 'translateY(0)', offset:0}),
+          style({opacity:1, transform: 'translateY(-15px)', offset:0.7}),
+          style({opacity:0, transform: 'translateY(100%)', offset:0}),
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  state: string = 'inactive';
+
+  toggleFocus(){
+    console.log('inside toggle focus');
+    this.state = (this.state ==='inactive' ? 'active' : 'inactive');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HowToComponent } from './how-to/how-to.component';
 import { ExamplesComponent } from './examples/examples.component';
@@ -14,11 +14,13 @@ import { routes } from './app.routes';
     AppComponent,
     HowToComponent,
     ExamplesComponent
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
     routes
   ],
   providers: [],
