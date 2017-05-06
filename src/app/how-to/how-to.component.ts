c11687e5a45211994e7e08414f89ce2fda777a48
Deepak Singla <thejavascriptways@gmail.com>
2017-05-06 15:19:40 -0500

    finished animations todo app.

--- src/app/how-to/how-to.component.ts
+++ src/app/how-to/how-to.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { pageTransition } from '../animations';
 
 @Component({
   selector: 'app-how-to',
   templateUrl: './how-to.component.html',
-  styleUrls: ['./how-to.component.scss']
+  styleUrls: ['./how-to.component.scss'],
+  animations:[ pageTransition ]
 })
 export class HowToComponent implements OnInit {
 
+  state : string ='in';
   constructor() { }
 
   ngOnInit() {
+    this.state = this.state === 'in'? 'out' : 'in';
   }
 
 }
